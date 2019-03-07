c1fef2bd8c1a410143c764abab99bb221a9dc4ca
Jeff Wiedemann <jeff@jeffwiedemann.com>
2019-03-07 12:23:33 +0000

    push

--- test_gps.py
+++ test_gps.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import serial
- 
+import time
 port = "/dev/serial0"
 
 ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
@@ -9,5 +9,6 @@ ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
 print "Looking for GPS Data"
 
 while True:
+   time.sleep(0.5)
    data = ser.read_until() 
    print data
